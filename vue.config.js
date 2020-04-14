21462da9ce5f5910298256ea48f647760822b5e6
Teemu Kallio <teemu.tuomas.oskari@gmail.com>
2020-04-14 07:03:38 +0200

    Clean up vue config

--- vue.config.js
+++ vue.config.js
@@ -3,10 +3,8 @@ module.exports = {
     proxy: {
       '^/http-bind': {
         target: 'http://localhost:8000',
-        // changeOrigin: true,
         logLevel: 'debug',
         xfwd: true,
-        ws: true,
       },
     },
   },
